f784bdae39c14e16226e1cb1c76b4925a0c7a3a9
AndreBoi02 <97614174+AndreBoi02@users.noreply.github.com>
2024-01-31 08:55:50 -0600

    Pool

---
+++ Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    private List<GameObject> availableObjectsList;
+    private List<GameObject> UnavailableObjectsList;
+
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int poolSize;
+
+    private void Awake()
+    {
+        availableObjectsList = new List<GameObject>();
+        UnavailableObjectsList = new List<GameObject>();
+    }
+
+    void Start()
+    {
+        CreateObjects(poolSize);
+    }
+
+    void CreateObjects(int poolSize)
+    {
+        GameObject tempObject;
+        for (int i = 0; i < poolSize; i++)
+        {
+            tempObject = Instantiate(prefab, Vector3.zero, Quaternion.identity, transform);
+            tempObject.SetActive(false);
+            availableObjectsList.Add(tempObject);
+        }
+    }
+
+    public GameObject RequestObject()
+    {
+        if (availableObjectsList.Count != 0)
+        {
+            GameObject requestedObject = availableObjectsList[0];
+            availableObjectsList.Remove(requestedObject);
+            UnavailableObjectsList.Add(requestedObject);
+            return requestedObject;
+        }
+        else
+        {
+            CreateObjects(1);            
+            return RequestObject();
+        }
+    }
+
+    public void DespawnObject(GameObject object2Despawn)
+    {
+        object2Despawn.SetActive(false);
+        availableObjectsList.Add(object2Despawn);
+        UnavailableObjectsList.Remove(object2Despawn);
+    }
+}
